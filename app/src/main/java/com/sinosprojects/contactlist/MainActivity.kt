0e5eeb6f02db09004f7cdfeddbd1c3824a1a916d
ning-sy210 <ningshy97@gmail.com>
2020-05-12 19:14:40 +0800

    Integrated Request Contact List Access Permission
    Application will now prompt user for contact list access permission before proceeding forward

--- app/src/main/java/com/sinosprojects/contactlist/MainActivity.kt
+++ app/src/main/java/com/sinosprojects/contactlist/MainActivity.kt
@@ -1,12 +1,68 @@
 package com.sinosprojects.contactlist
 
+import android.app.AlertDialog
+import android.content.Context
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_READ_CONTACTS_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val contactList = findViewById<RecyclerView>(R.id.contactList)
+
+        if (!hasContactListAccessPermission()) {
+            requestContactListAccessPermission()
+        }
+        readContactList()
+    }
+
+    fun readContactList() {
+
+    }
+
+    private fun hasContactListAccessPermission() : Boolean {
+        return ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestContactListAccessPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.READ_CONTACTS)) {
+            AlertDialog.Builder(this)
+                    .setTitle("Permission Required")
+                    .setMessage("This permission is needed in order to display your contacts.")
+                    .setPositiveButton("Okay", DialogInterface.OnClickListener { dialog, which ->
+                        ActivityCompat.requestPermissions(this,
+                                arrayOf(android.Manifest.permission.READ_CONTACTS), REQUEST_READ_CONTACTS_CODE)
+                    })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+                        dialog.dismiss()
+                    })
+                    .create()
+                    .show()
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.READ_CONTACTS), REQUEST_READ_CONTACTS_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_READ_CONTACTS_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
